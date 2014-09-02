f38e8ab198ad579de9664af9bf3ecd6155c536a2
Scott McCulley <smcculley@firm58.com>
2014-09-01 23:17:02 -0400

    Added remarks

--- n3rfgun.ino
+++ n3rfgun.ino
@@ -1,15 +1,26 @@
 /*
- *  N3RF
+ *  N3RFGUN
  *
- *  On Adafruit Trinket 5v
+ *  When magazine is inserted into the gun, the power switch is
+ *  activated, and the circuit is powered by 4x c-batteries (6v).
+ *  The gun should be silent, but the NeoPixels will start with the
+ *  rainbow pattern, showing that the gun is active.
+ *  When the speed switch is pressed, the accelerator motors will start
+ *  to spin at 50%. Another click goes to 75%, and a third brings it
+ *  to full power. One last click drops back to 0%.
+ *  The main trigger will not fire unless the speed switch is pressed
+ *  and held in place.  When you click to your desired speed, simply
+ *  hold the button, then fire at will!
+ *  Pressing the trigger button will activate the loader, which places
+ *  a dart in the accelerator, and launches it out of the gun.
+ *  This is a fully automatic gun, so it will fire until the darts are
+ *  gone, or you let up on the trigger.
+ *  Pressing the trigger also activates a lightning animation sequence
+ *  on the NeoPixels. This simulates the muzzle flash, and looks cool
+ *  in the dark or at night!
  *
- *  TRG pin is the gun trigger.  When pressed, the trinket checks the status
- *  of the ARM pin.  When LOW (closed), the gun is armed, and can be fired.
- *  In the case that ARM and TRG are both LOW, the LOD pin will be activated,
- *  sending the dart into the already running ACT motors.
- *  When ARM pin goes from LOW to HIGH (gun just fired) then also start
- *  the gunfire animation.
- *  https://github.com/sKr0d/n3rfgun
+ *  https://github.com/sKr0d/n3rfgun/wiki
+ *  Scott and Alex McCulley, September, 2014
  */
 
 int numpix = 6; // number of pixels in the chain
@@ -22,21 +33,21 @@ int numpix = 6; // number of pixels in the chain
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(numpix, PIN, NEO_GRB + NEO_KHZ800);
 
 int TRG = 4;   // trigger pin
-int ARM = 3;   // armed pin
+int SPD = 3;   // speed pin
 int ACT = 0;   // action pin
 int LOD = 1;   // loader pin
 int NEO = 2;   // neopixel pin
 
 int ACTS = 0;   // action speeed
 int LODS = 240; // loader speed
-int REL = 0;   //release speed button
+int REL = 0;    // release speed button
 
-int i = 0;
-int k = 0;
+int i = 0;	// for loop
+int k = 0;	// for loop
  
 void setup() {
    pinMode(TRG, INPUT);
-   pinMode(ARM, INPUT);
+   pinMode(SPD, INPUT);
    pinMode(ACT, OUTPUT);
    pinMode(LOD, OUTPUT);
    pinMode(NEO, OUTPUT);
@@ -47,12 +58,12 @@ void setup() {
 }
  
 void loop() {
-  analogWrite(ACT, ACTS);  
-   rainbow(20);
+   analogWrite(ACT, ACTS);	// set the acceleration motor speed
+   rainbow(20);			// start the rainbow animation
 }
 
-void arm() {
-  if (digitalRead(ARM) == LOW && REL == 1) {
+void spd() {
+  if (digitalRead(SPD) == LOW && REL == 1) {
     switch (ACTS) {
       case 0:
         ACTS = 128; // 50%
@@ -71,7 +82,7 @@ void arm() {
     delay(40);
     REL = 0;
   } else {
-    if (digitalRead(ARM) == HIGH) {
+    if (digitalRead(SPD) == HIGH) {
       REL = 1;
     }
   }
@@ -89,8 +100,8 @@ void rainbow(uint8_t wait) { // make pretty rainbow colors
   uint16_t i, j;
 
   for(j=0; j<256; j=j+4) {
-    arm();
-    trig();
+    spd();	// check for press of speed button
+    trig();	// check for press of trigger button
     for(i=0; i<pixels.numPixels(); i++) {
       pixels.setPixelColor(i, Wheel((i+j) & 255));
     }
